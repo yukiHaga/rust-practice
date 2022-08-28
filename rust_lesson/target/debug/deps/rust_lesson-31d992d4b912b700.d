c23f5033e8a649e5b7671828f0f5d5c7e975baf0
yukiHaga <oceansthirteen7510@gmail.com>
2022-08-29 00:05:23 +0900

    initial commit

---
+++ rust_lesson/target/debug/deps/rust_lesson-31d992d4b912b700.d
@@ -0,0 +1,5 @@
+/Users/yuuki_haga/repos/rust/rust-practice/rust_lesson/target/debug/deps/rust_lesson-31d992d4b912b700: src/main.rs
+
+/Users/yuuki_haga/repos/rust/rust-practice/rust_lesson/target/debug/deps/rust_lesson-31d992d4b912b700.d: src/main.rs
+
+src/main.rs:
---
+++ rust_lesson/target/debug/rust_lesson.d
@@ -0,0 +1,1 @@
+/Users/yuuki_haga/repos/rust/rust-practice/rust_lesson/target/debug/rust_lesson: /Users/yuuki_haga/repos/rust/rust-practice/rust_lesson/src/main.rs
